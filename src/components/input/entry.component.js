609db8f0417d0583f5f4aeb46ff4365b3f05407b
algojedi <vicbanwait@hotmail.com>
2019-08-28 16:55:04 -0400

    first commit

---
+++ src/components/input/entry.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './entry.css';
+
+const Entry = () => {
+    return ( 
+        
+        <div>
+            <input type='text' placeholder='enter here'/>
+            <button type='submit'>submit</button>
+        </div>
+     );
+}
+ 
+export default Entry;
