c8840feb4eaa26720accaae42a7336ed44a5d9b0
algojedi <vicbanwait@hotmail.com>
2019-09-15 11:12:08 -0400

    adding signin

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.component';
 import List from './components/list/list-container'
+import Signin from './components/signin/signin';
 
 
 function App() {
   return (
     <div className="App">
+      <Signin/>
       <Header/>
       <List/>
     </div>
